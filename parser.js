737d65c6dd2434f5d8d5a851276f336441b14164
Jon Kolman <jonathankolman@gmail.com>
2017-05-18 16:43:27 -0400

    remove console.log

--- parser.js
+++ parser.js
@@ -1,6 +1,5 @@
 import cheerio from 'cheerio'
 export const parseProfile = profile => {
-  console.log('parsing profile')
   return new Promise((resolve, reject) => {
     const $ = cheerio.load(profile)
     let name = $('.profile__meta-name').first().text().replace(/(\r\n|\n|\r)/gm,"").split(' ').join("").trim()
@@ -17,5 +16,16 @@ export const parseProfile = profile => {
       phoneNumber,
       email
     })
+  })
+}
+
+export const findVerificationToken = page => {
+  return new Promise((resolve, reject) => {
+    const $ = cheerio.load(page)
+    let token = $('#content > div.row > div:nth-child(1) > div > div.panel-body > form > input[type="hidden"]:nth-child(1)').attr('value')
+    if(token) {
+      resolve(token)
+    }
+    reject('could not find token')
   })
 }
